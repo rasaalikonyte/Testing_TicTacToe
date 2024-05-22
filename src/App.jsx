3598a0b4322e368da2738dcc83d34719d4cc79a8
Your Name <rasa.alikonyte@gmail.com>
2024-05-22 12:57:58 +0300

    vite app

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import Game from "./components/Game";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Game />
+    </div>
+  );
+};
+
+export default App;
+
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/rasaalikonyte/Testing_TicTacToe.git
+// git push -u origin main
