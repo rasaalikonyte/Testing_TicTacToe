7d08243c98d27b0998c8866d780de765d1cd5506
Your Name <rasa.alikonyte@gmail.com>
2024-05-22 15:22:55 +0300

    added new test

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/rasaalikonyte/Testing_TicTacToe.git
+// git push -u origin main
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,9 +1,38 @@
-describe("Check for text Next player: X", () => {
+describe("My New Cypress Tests for tic-tac-toe", () => {
   it("should find a text Next player: X", () => {
     // Visit the page you want to test
     cy.visit("http://localhost:5173/");
 
-    // Check for the header with text 'hello'
+    // Check for the text "Next player: X"
     cy.contains("div", "Next player: X").should("exist");
   });
+
+  // Check for the text "Next player: O"
+  it("should visit the home page and check the title", () => {
+    cy.visit("http://localhost:5173/");
+    cy.contains("div", "Next player: O").should("exist");
+  });
+
+  // // Check for the text "Next player: O"
+  // it("should visit the home page and check the title", () => {
+  //   cy.visit("http://localhost:5173/");
+  //   cy.contains("div", "Next player: O").should("exist");
+  // });
 });
+
+// describe("Square Component", () => {
+//   it("renders with the correct value", () => {
+//     const value = "X";
+//     mount(<Square value={value} onClick={() => {}} />);
+//     cy.visit("http://localhost:5173/");
+//     cy.get("button.square").should("contain", value);
+//   });
+
+//   it("triggers onClick event when clicked", () => {
+//     const onClick = cy.stub().as("onClick");
+//     mount(<Square value="O" onClick={onClick} />);
+//     cy.visit("http://localhost:5173/");
+//     cy.get("button.square").click();
+//     cy.get("@onClick").should("have.been.calledOnce");
+//   });
+// });
