faa4a1bc2b1f6425773a271c9cbb576b0885694c
Your Name <rasa.alikonyte@gmail.com>
2024-05-22 18:51:09 +0300

    cy tests updated

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,38 +1,63 @@
-describe("My New Cypress Tests for tic-tac-toe", () => {
-  it("should find a text Next player: X", () => {
-    // Visit the page you want to test
-    cy.visit("http://localhost:5173/");
-
-    // Check for the text "Next player: X"
-    cy.contains("div", "Next player: X").should("exist");
-  });
-
-  // Check for the text "Next player: O"
-  it("should visit the home page and check the title", () => {
-    cy.visit("http://localhost:5173/");
-    cy.contains("div", "Next player: O").should("exist");
-  });
-
-  // // Check for the text "Next player: O"
-  // it("should visit the home page and check the title", () => {
-  //   cy.visit("http://localhost:5173/");
-  //   cy.contains("div", "Next player: O").should("exist");
-  // });
-});
-
-// describe("Square Component", () => {
-//   it("renders with the correct value", () => {
-//     const value = "X";
-//     mount(<Square value={value} onClick={() => {}} />);
+// describe("My New Cypress Tests for tic-tac-toe", () => {
+//   it("should find a text Next player: X", () => {
 //     cy.visit("http://localhost:5173/");
-//     cy.get("button.square").should("contain", value);
+//     cy.contains("div", "Next player: X").should("exist");
 //   });
-
-//   it("triggers onClick event when clicked", () => {
-//     const onClick = cy.stub().as("onClick");
-//     mount(<Square value="O" onClick={onClick} />);
-//     cy.visit("http://localhost:5173/");
-//     cy.get("button.square").click();
-//     cy.get("@onClick").should("have.been.calledOnce");
-//   });
+// it("should find a text Next player: O", () => {
+//   // Visit the page you want to test
+//   cy.visit("http://localhost:5173/");
+//   // Check for the text "Next player: O"
+//   cy.contains("div", "Next player: O").should("exist");
 // });
+// });
+
+describe("Cypress Tests for Tic-Tac-Toe Game", () => {
+  beforeEach(() => {
+    // Assuming your game is hosted at localhost:3000
+    cy.visit("http://localhost:5173/");
+  });
+
+  it("should display the initial game state", () => {
+    cy.get(".square").should("have.length", 9);
+    cy.get(".square").each(($el) => {
+      cy.wrap($el).should("be.empty");
+    });
+    cy.get(".game-info div").should("contain", "Next player: X");
+  });
+
+  it("should handle a single move", () => {
+    cy.get(".square").eq(0).click();
+    cy.get(".square").eq(0).should("contain", "X");
+    cy.get(".game-info div").should("contain", "Next player: O");
+  });
+
+  it("should declare a winner", () => {
+    cy.get(".square").eq(0).click(); // X
+    cy.get(".square").eq(1).click(); // O
+    cy.get(".square").eq(3).click(); // X
+    cy.get(".square").eq(4).click(); // O
+    cy.get(".square").eq(6).click(); // X wins
+    cy.get(".game-info div").should("contain", "Winner: X");
+  });
+
+  it("should reset the game", () => {
+    cy.get(".square").eq(0).click(); // X
+    cy.get(".square").eq(1).click(); // O
+    cy.get("button").contains("Go to game start").click();
+    cy.get(".square").each(($el) => {
+      cy.wrap($el).should("be.empty");
+    });
+    cy.get(".game-info div").should("contain", "Next player: X");
+  });
+
+  it("should jump to a specific move", () => {
+    cy.get(".square").eq(0).click(); // X
+    cy.get(".square").eq(1).click(); // O
+    cy.get(".square").eq(2).click(); // X
+    cy.get("button").contains("Go to move #1").click();
+    cy.get(".square").eq(0).should("contain", "X");
+    cy.get(".square").eq(1).should("be.empty");
+    cy.get(".square").eq(2).should("be.empty");
+    cy.get(".game-info div").should("contain", "Next player: O");
+  });
+});
